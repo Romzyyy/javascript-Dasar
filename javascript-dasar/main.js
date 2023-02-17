e14c1db49683f7d63ce060100d0d5da772d56a8f
romzyyy <fijay0117@gmail.com>
2023-02-17 21:29:27 +0700

    move to array basic

---
+++ javascript-dasar/main.js
@@ -0,0 +1,89 @@
+// // alert('hello word');
+// // console.log('heloooo');
+// // prompt('masukan nama anda');
+
+// // let usia = 30;
+// // usia = 35;
+// // console.log('usia kamu adalah ' + usia);
+// // alert('usia kamu adalah ' + usia);
+
+// // let usia = prompt('berapa usia anda?');
+// // alert('usia anda adalah' + usia);
+// let nama = 'dea afrizal';
+// let usia = 30;
+// let tinggiBadan = 173.5;
+// let beratBadan;
+// let pacar = 2;
+
+// beratBadan = 60;
+
+// // if (pacar == null) {
+// //     pacar = 'belum punya';
+// // } else {
+// //     pacar = 'sudah punya';
+// // }
+// switch (pacar) {
+//     case 1:
+//         pacar = 'punya 1 ajh';
+//         break;
+//     case 2:
+//         pacar = 'punya 2 anjay';
+//         break;
+//     default:
+//         pacar = 'belum punya';
+//         break;
+// }
+
+// let saldoAwal = 5000;
+// let saldoTambahan = 8000;
+// const hutang = 3000;
+// const saldo = saldoAwal + saldoTambahan - hutang;
+
+// // alert('nama anda adalah ' + nama + ' usia anda adalah' + usia);
+// alert(
+//     `nama saya ${nama} usia saya ${usia} tinggi badan saya ${tinggiBadan}cm berat badan saya ${beratBadan}kg pacara saya ${pacar}`
+// );
+// alert(
+//     `saldo awal saya ${saldoAwal} saldo saya akan bertambah ${saldoTambahan} jadi total saldo saya adalah ${saldo}`
+// );
+
+// let namaGuru = ['jodi', 'bunga', 'rahman'];
+// namaGuru.push('dea', 'fikri');
+// namaGuru.shift();
+// namaGuru.pop();
+// alert(namaGuru);
+
+// let namaGuru = [];
+
+// namaGuru[0] = 'dea';
+// namaGuru[1] = 'fikri';
+// namaGuru.pop();
+
+// alert(namaGuru);
+
+// console.log('nama saya dea');
+
+// const namaGuru = ['dea', 'fikri', 'retno', 'bunga'];
+// console.log(namaGuru.length);
+
+// for (i = 0; i < namaGuru.length; i++) {
+//     console.log(namaGuru[i]);
+// }
+
+// let i = 0;
+// // while (i < 10) {
+// //     i++;
+// //     console.log('dea afrizal');
+// // }
+// do {
+//     i++;
+//     console.log('dea afrizal');
+// } while (i < 10);
+
+//TUGAS
+//1. promt ununtk mengetahui saldo akhir dari apa yang di inputkan oleh user
+//2. menentukan hari dari tanggal yang ada saat ini di pc kalian
+
+//clue
+let hari = new Date().getDate();
+alert(`hari ini adalah ${hari}`);
